d12cdcf4e440a91aa30c509b136b083028d90c4d
Siying Dong <siying.d@fb.com>
2018-07-26 11:03:44 -0700

    DBImpl::IngestExternalFile() should grab mutex when releasing file number in failure case (#4189)
    Summary:
    995fcf757319da9cf12eca2df83a6fba4db0ebe4 has a bug: ReleaseFileNumberFromPendingOutputs() added is not protected by the DB mutex. Fix it by grabbing the lock for this operation.
    Pull Request resolved: https://github.com/facebook/rocksdb/pull/4189
    
    Differential Revision: D9015447
    
    Pulled By: siying
    
    fbshipit-source-id: b8506e09a96c3f95a6fe32b5ca5fcdb9bee88937

--- db/db_impl.cc
+++ db/db_impl.cc
@@ -2865,6 +2865,7 @@ Status DBImpl::IngestExternalFile(
 
   status = ingestion_job.Prepare(external_files);
   if (!status.ok()) {
+    InstrumentedMutexLock l(&mutex_);
     ReleaseFileNumberFromPendingOutputs(pending_output_elem);
     return status;
   }
