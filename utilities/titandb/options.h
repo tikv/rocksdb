9cd8875937070ac30cbbf9a1c072a9ed1ab30b1a
Huachao Huang <huachao.huang@gmail.com>
2018-08-08 15:05:29 +0800

    titandb: add check commands to Makefile and add some comments (#27)

--- utilities/titandb/options.h
+++ utilities/titandb/options.h
@@ -6,6 +6,9 @@ namespace rocksdb {
 namespace titandb {
 
 struct TitanDBOptions {
+  // The directory to store data specific to TitanDB alongside with
+  // the base DB.
+  //
   // Default: {dbname}/titandb
   std::string dirname;
 
@@ -15,10 +18,19 @@ struct TitanDBOptions {
   // Default: 4096
   uint64_t min_blob_size {4096};
 
+  // The maximum open blob files in the blob file cache.
+  //
+  // Default: 1 << 20
   uint64_t max_open_files {1 << 20};
 
+  // The compression algorithm used to compress blob records in blob files.
+  //
+  // Default: kNoCompression
   CompressionType blob_file_compression {kNoCompression};
 
+  // The desirable blob file size. This is not a hard limit but a wish.
+  //
+  // Default: 256MB
   uint64_t blob_file_target_size {256 << 20};
 
   std::string ToString() const;
