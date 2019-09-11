385af5d9dba893a280a93601325c94ce4aa9e4cb
zhangyiwen <zzyyyww9595@gmail.com>
2019-09-11 17:19:04 +0800

    remove build dir

--- cmake-build-debug/build_version.cc
+++
@@ -1,4 +0,0 @@
-#include "build_version.h"
-const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:@f5d7bd6c8a46805b5fe82f95f70506927a209f06@";
-const char* rocksdb_build_git_date = "rocksdb_build_git_date:@2019/09/09 07:08:30@";
-const char* rocksdb_build_compile_date = __DATE__;
