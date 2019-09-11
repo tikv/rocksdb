b7b1aa7e8eddc35c496a867a504bf4198e937188
zhangyiwen <zzyyyww9595@gmail.com>
2019-09-11 19:11:26 +0800

    fix compile error

--- src.mk
+++ src.mk
@@ -34,7 +34,6 @@ LIB_SOURCES =                                                   \
   db/flush_job.cc                                               \
   db/flush_scheduler.cc                                         \
   db/forward_iterator.cc                                        \
-  db/in_memory_stats_history.cc                                 \
   db/internal_stats.cc                                          \
   db/logs_with_prep_tracker.cc                                  \
   db/log_reader.cc                                              \
