7ce624279df6f2d16ce6e309780041308741afac
Connor1996 <zbk602423539@gmail.com>
2019-11-21 17:50:28 +0800

    fix warning
    Signed-off-by: Connor1996 <zbk602423539@gmail.com>

--- db/compaction_picker.cc
+++ db/compaction_picker.cc
@@ -1164,7 +1164,7 @@ class LevelCompactionBuilder {
                             const MutableCFOptions& mutable_cf_options,
                             int level);
 
-  static const int kMinFilesForIntraL0Compaction = 4;
+  // static const int kMinFilesForIntraL0Compaction = 4;
 };
 
 void LevelCompactionBuilder::PickExpiredTtlFiles() {
