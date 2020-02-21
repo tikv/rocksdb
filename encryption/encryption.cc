151be5031c1d3ecd28a4816737df52052ef171a9
Yi Wu <yiwu@pingcap.com>
2020-02-21 15:54:36 -0800

    env
    Signed-off-by: Yi Wu <yiwu@pingcap.com>

---
+++ encryption/encryption.cc
@@ -0,0 +1,203 @@
+#include "rocksdb/encryption.h"
+
+#include "util/string_util.h"
+
+namespace rocksdb {
+namespace encryption {
+
+Status NewAESCTRCipherStream(EncryptionMethod method, const std::string& key,
+                             const std::string& iv,
+                             std::unique_ptr<AESCTRCipherStream>* result) {
+  assert(result != nullptr);
+  size_t key_size = 0;
+  switch (method) {
+    case EncryptionMethod::kAES128_CTR:
+      key_size = 16;
+      break;
+    case EncryptionMethod::kAES192_CTR:
+      key_size = 24;
+      break;
+    case EncryptionMethod::kAES256_CTR:
+      key_size = 32;
+      break;
+    default:
+      return Status::InvalidArgument("Unsupported encryption method: " +
+                                     ToString(static_cast<int>(method)));
+  }
+  if (key.size() != key_size) {
+    return Status::InvalidArgument("Encryption key size mismatch. " +
+                                   ToString(key.size()) + "(actual) vs. " +
+                                   ToString(key_size) + "(expected).");
+  }
+  if (iv.size() != AES_BLOCK_SIZE) {
+    return Status::InvalidArgument(
+        "iv size not equal to block cipher block size: " + ToString(iv.size()) +
+        "(actual) vs. " + ToString(AES_BLOCK_SIZE) + "(expected).");
+  }
+  result->reset(new AESCTRCipherStream(key, iv));
+  return Status::OK();
+}
+
+Status AESEncryptionProvider::CreateCipherStream(
+    const std::string& fname, const EnvOptions& /*options*/, Slice& /*prefix*/,
+    std::unique_ptr<BlockAccessCipherStream>* result) {
+  assert(result != nullptr);
+  FileInfo file_info;
+  Status s = key_manager_->GetInfoForFile(fname, &file_info);
+  if (!s.ok()) {
+    return s;
+  }
+  std::unique_ptr<AESCTRCipherStream> cipher_stream;
+  s = NewAESCTRCipherStream(file_info.method, file_info.key, file_info.iv,
+                            &cipher_stream);
+  if (!s.ok()) {
+    return s;
+  }
+  *result = std::move(cipher_stream);
+  return Status::OK();
+}
+
+Status KeyManagedEncryptedEnv::NewSequentialFile(
+    const std::string& fname, std::unique_ptr<SequentialFile>* result,
+    const EnvOptions& options) {
+  FileInfo file_info;
+  Status s = key_manager_->GetInfoForFile(fname, &file_info);
+  if (!s.ok()) {
+    return s;
+  }
+  switch (file_info.method) {
+    case EncryptionMethod::kPlaintext:
+      return target()->NewSequentialFile(fname, result, options);
+    case EncryptionMethod::kAES128_CTR:
+    case EncryptionMethod::kAES192_CTR:
+    case EncryptionMethod::kAES256_CTR:
+      return encrypted_env_->NewSequentialFile(fname, result, options);
+    default:
+      return Status::InvalidArgument(
+          "Unsupported encryption method: " +
+          ToString(static_cast<int>(file_info.method)));
+  }
+}
+
+Status KeyManagedEncryptedEnv::NewRandomAccessFile(
+    const std::string& fname, std::unique_ptr<RandomAccessFile>* result,
+    const EnvOptions& options) {
+  FileInfo file_info;
+  Status s = key_manager_->GetInfoForFile(fname, &file_info);
+  if (!s.ok()) {
+    return s;
+  }
+  switch (file_info.method) {
+    case EncryptionMethod::kPlaintext:
+      return target()->NewRandomAccessFile(fname, result, options);
+    case EncryptionMethod::kAES128_CTR:
+    case EncryptionMethod::kAES192_CTR:
+    case EncryptionMethod::kAES256_CTR:
+      return encrypted_env_->NewRandomAccessFile(fname, result, options);
+    default:
+      return Status::InvalidArgument(
+          "Unsupported encryption method: " +
+          ToString(static_cast<int>(file_info.method)));
+  }
+}
+
+Status KeyManagedEncryptedEnv::NewWritableFile(
+    const std::string& fname, std::unique_ptr<WritableFile>* result,
+    const EnvOptions& options) {
+  FileInfo file_info;
+  Status s = key_manager_->NewFile(fname, &file_info);
+  if (!s.ok()) {
+    return s;
+  }
+  switch (file_info.method) {
+    case EncryptionMethod::kPlaintext:
+      return target()->NewWritableFile(fname, result, options);
+    case EncryptionMethod::kAES128_CTR:
+    case EncryptionMethod::kAES192_CTR:
+    case EncryptionMethod::kAES256_CTR:
+      return encrypted_env_->NewWritableFile(fname, result, options);
+    default:
+      return Status::InvalidArgument(
+          "Unsupported encryption method: " +
+          ToString(static_cast<int>(file_info.method)));
+  }
+}
+
+Status KeyManagedEncryptedEnv::ReopenWritableFile(
+    const std::string& fname, std::unique_ptr<WritableFile>* result,
+    const EnvOptions& options) {
+  FileInfo file_info;
+  Status s = key_manager_->GetInfoForFile(fname, &file_info);
+  if (!s.ok()) {
+    return s;
+  }
+  switch (file_info.method) {
+    case EncryptionMethod::kPlaintext:
+      return target()->ReopenWritableFile(fname, result, options);
+    case EncryptionMethod::kAES128_CTR:
+    case EncryptionMethod::kAES192_CTR:
+    case EncryptionMethod::kAES256_CTR:
+      return encrypted_env_->ReopenWritableFile(fname, result, options);
+    default:
+      return Status::InvalidArgument(
+          "Unsupported encryption method: " +
+          ToString(static_cast<int>(file_info.method)));
+  }
+}
+
+Status KeyManagedEncryptedEnv::ReuseWritableFile(
+    const std::string& fname, const std::string& old_fname,
+    std::unique_ptr<WritableFile>* result, const EnvOptions& options) {
+  FileInfo file_info;
+  Status s = key_manager_->NewFile(fname, &file_info);
+  if (!s.ok()) {
+    return s;
+  }
+  switch (file_info.method) {
+    case EncryptionMethod::kPlaintext:
+      return target()->ReuseWritableFile(fname, old_fname, result, options);
+    case EncryptionMethod::kAES128_CTR:
+    case EncryptionMethod::kAES192_CTR:
+    case EncryptionMethod::kAES256_CTR:
+      return encrypted_env_->ReuseWritableFile(fname, old_fname, result,
+                                               options);
+    default:
+      return Status::InvalidArgument(
+          "Unsupported encryption method: " +
+          ToString(static_cast<int>(file_info.method)));
+  }
+}
+
+Status KeyManagedEncryptedEnv::NewRandomRWFile(
+    const std::string& fname, std::unique_ptr<RandomRWFile>* result,
+    const EnvOptions& options) {
+  FileInfo file_info;
+  Status s = key_manager_->NewFile(fname, &file_info);
+  if (!s.ok()) {
+    return s;
+  }
+  switch (file_info.method) {
+    case EncryptionMethod::kPlaintext:
+      return target()->NewRandomRWFile(fname, result, options);
+    case EncryptionMethod::kAES128_CTR:
+    case EncryptionMethod::kAES192_CTR:
+    case EncryptionMethod::kAES256_CTR:
+      return encrypted_env_->NewRandomRWFile(fname, result, options);
+    default:
+      return Status::InvalidArgument(
+          "Unsupported encryption method: " +
+          ToString(static_cast<int>(file_info.method)));
+  }
+}
+
+Env* NewKeyManagedEncryptedEnv(Env* base_env,
+                               std::shared_ptr<KeyManager>& key_manager) {
+  std::unique_ptr<AESEncryptionProvider> provider(
+      new AESEncryptionProvider(key_manager.get()));
+  std::unique_ptr<Env> encrypted_env(NewEncryptedEnv(base_env, provider.get()));
+  return new KeyManagedEncryptedEnv(base_env, key_manager, std::move(provider),
+                                    std::move(encrypted_env));
+}
+
+}  // namespace encryption
+}  // namespace rocksdb
---
+++ include/rocksdb/encryption.h
@@ -0,0 +1,178 @@
+#pragma once
+
+#include <memory>
+#include <string>
+
+#include <openssl/aes.h>
+
+#include "rocksdb/env_encryption.h"
+#include "util/coding.h"
+
+namespace rocksdb {
+namespace encryption {
+
+enum class EncryptionMethod : int {
+  kUnknown = 0,
+  kPlaintext = 1,
+  kAES128_CTR = 2,
+  kAES192_CTR = 3,
+  kAES256_CTR = 4,
+};
+
+struct FileInfo {
+  EncryptionMethod method = EncryptionMethod::kUnknown;
+  std::string key;
+  std::string iv;
+};
+
+class KeyManager {
+ public:
+  virtual ~KeyManager() = default;
+
+  virtual Status GetInfoForFile(const std::string& fname,
+                                FileInfo* file_info) = 0;
+  virtual Status NewFile(const std::string& fname, FileInfo* file_info) = 0;
+  virtual Status DeleteFile(const std::string& fname) = 0;
+};
+
+class AESBlockCipher final : public BlockCipher {
+ public:
+  AESBlockCipher(Slice key) {
+    AES_set_encrypt_key(reinterpret_cast<const unsigned char*>(key.data()),
+                        static_cast<int>(key.size()), &encrypt_key_);
+    AES_set_decrypt_key(reinterpret_cast<const unsigned char*>(key.data()),
+                        static_cast<int>(key.size()), &decrypt_key_);
+  }
+
+  size_t BlockSize() override {
+    return AES_BLOCK_SIZE;  // 16
+  }
+
+  Status Encrypt(char* data) override {
+    AES_encrypt(reinterpret_cast<unsigned char*>(data),
+                reinterpret_cast<unsigned char*>(data), &encrypt_key_);
+    return Status::OK();
+  }
+
+  Status Decrypt(char* data) override {
+    AES_decrypt(reinterpret_cast<unsigned char*>(data),
+                reinterpret_cast<unsigned char*>(data), &decrypt_key_);
+    return Status::OK();
+  }
+
+ private:
+  AES_KEY encrypt_key_;
+  AES_KEY decrypt_key_;
+};
+
+class AESCTRCipherStream : public BlockAccessCipherStream {
+ public:
+  static constexpr size_t kNonceSize = AES_BLOCK_SIZE - sizeof(uint64_t);  // 8
+
+  AESCTRCipherStream(const std::string& key, const std::string& iv)
+      : block_cipher_(key),
+        nonce_(iv, 0, kNonceSize),
+        initial_counter_(
+            *reinterpret_cast<const uint64_t*>(iv.data() + kNonceSize)) {}
+
+  size_t BlockSize() override {
+    return AES_BLOCK_SIZE;  // 16
+  }
+
+ protected:
+  void AllocateScratch(std::string& scratch) override {
+    scratch.reserve(BlockSize());
+  }
+
+  Status EncryptBlock(uint64_t block_index, char* data,
+                      char* scratch) override {
+    memcpy(scratch, nonce_.data(), kNonceSize);
+    EncodeFixed64(scratch + kNonceSize, block_index + initial_counter_);
+    Status s = block_cipher_.Encrypt(scratch);
+    if (!s.ok()) {
+      return s;
+    }
+    for (size_t i = 0; i < AES_BLOCK_SIZE; i++) {
+      data[i] = data[i] ^ scratch[i];
+    }
+    return Status::OK();
+  }
+
+  Status DecryptBlock(uint64_t block_index, char* data,
+                      char* scratch) override {
+    return EncryptBlock(block_index, data, scratch);
+  }
+
+ private:
+  AESBlockCipher block_cipher_;
+  std::string nonce_;
+  uint64_t initial_counter_;
+};
+
+extern Status NewAESCTRCipherStream(
+    std::unique_ptr<AESBlockCipher>&& block_cipher, const std::string& iv);
+
+class AESEncryptionProvider : public EncryptionProvider {
+ public:
+  AESEncryptionProvider(KeyManager* key_manager) : key_manager_(key_manager) {}
+  virtual ~AESEncryptionProvider() = default;
+
+  size_t GetPrefixLength() override { return 0; }
+
+  Status CreateNewPrefix(const std::string& /*fname*/, char* /*prefix*/,
+                         size_t /*prefix_length*/) override {
+    return Status::OK();
+  }
+
+  Status CreateCipherStream(
+      const std::string& fname, const EnvOptions& options, Slice& prefix,
+      std::unique_ptr<BlockAccessCipherStream>* result) override;
+
+ private:
+  KeyManager* key_manager_;
+};
+
+class KeyManagedEncryptedEnv : public EnvWrapper {
+ public:
+  KeyManagedEncryptedEnv(Env* base_env,
+                         std::shared_ptr<KeyManager>& key_manager,
+                         std::unique_ptr<AESEncryptionProvider>&& provider,
+                         std::unique_ptr<Env>&& encrypted_env)
+      : EnvWrapper(base_env),
+        key_manager_(key_manager),
+        provider_(std::move(provider)),
+        encrypted_env_(std::move(encrypted_env)) {}
+
+  virtual ~KeyManagedEncryptedEnv() = default;
+
+  Status NewSequentialFile(const std::string& fname,
+                           std::unique_ptr<SequentialFile>* result,
+                           const EnvOptions& options) override;
+  Status NewRandomAccessFile(const std::string& fname,
+                             std::unique_ptr<RandomAccessFile>* result,
+                             const EnvOptions& options) override;
+  Status NewWritableFile(const std::string& fname,
+                         std::unique_ptr<WritableFile>* result,
+                         const EnvOptions& options) override;
+  Status ReopenWritableFile(const std::string& fname,
+                            std::unique_ptr<WritableFile>* result,
+                            const EnvOptions& options) override;
+  Status ReuseWritableFile(const std::string& fname,
+                           const std::string& old_fname,
+                           std::unique_ptr<WritableFile>* result,
+                           const EnvOptions& options) override;
+  Status NewRandomRWFile(const std::string& fname,
+                         std::unique_ptr<RandomRWFile>* result,
+                         const EnvOptions& options) override;
+
+ private:
+  const std::shared_ptr<KeyManager> key_manager_;
+  const std::unique_ptr<AESEncryptionProvider> provider_;
+  const std::unique_ptr<Env> encrypted_env_;
+};
+
+extern Env* NewKeyManagedEncryptedEnv(Env* base_env,
+                                      std::shared_ptr<KeyManager>& key_manager);
+
+}  // namespace encryption
+}  // namespace rocksdb
