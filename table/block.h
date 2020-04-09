48acb2dba6ef145c46d99e7a4c1e95206a95c323
Yi Wu <yiwu@pingcap.com>
2020-04-08 21:19:49 -0700

    Fix wrong key being read on ingested file with global seqno and delta encoding (#6669)
    Summary:
    On reading an ingested SST file, `DataBlockIter` will replace seqno encoded in a key with global seqno. However, if the original seqno was part of the prefix used for the next key, the global seqno is by mistake used as part of the prefix to construct the next key, causing wrong result being returned. Although at this point it is only software error while data in the file is not corrupted, the issue can further cause compaction output out of order and corrupted result when the ingested SST participated in compaction. Fixing the issue by save the actual seqno and restore it before the key being used as prefix to construct next key.
    
    The unit test is by Little-Wallace from https://github.com/facebook/rocksdb/issues/6666. Fixing https://github.com/facebook/rocksdb/issues/6666.
    Pull Request resolved: https://github.com/facebook/rocksdb/pull/6669
    
    Test Plan:
    New unit test
    
    Signed-off-by: Yi Wu <yiwu@pingcap.com>
    
    Reviewed By: cheng-chang
    
    Differential Revision: D20931808
    
    Pulled By: ajkr
    
    fbshipit-source-id: f01959c35d6a493954dca981663766c7a5a9e8ab

--- table/block.h
+++ table/block.h
@@ -291,6 +291,11 @@ class BlockIter : public InternalIterator {
   // whether the block data is guaranteed to outlive this iterator
   bool block_contents_pinned_;
   SequenceNumber global_seqno_;
+  // Save the actual sequence before replaced by global seqno, which potentially
+  // is used as part of prefix of delta encoding.
+  SequenceNumber stored_seqno_ = 0;
+  // Save the value type of key_. Used to restore stored_seqno_.
+  ValueType stored_value_type_ = kMaxValue;
 
  public:
   // Return the offset in data_ just past the end of the current entry.
