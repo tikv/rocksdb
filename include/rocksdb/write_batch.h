f215462bd6a34d587748f45675a4535b85f8a325
Little-Wallace <bupt2013211450@gmail.com>
2020-06-18 11:11:12 +0800

    add more interface
    Signed-off-by: Little-Wallace <bupt2013211450@gmail.com>

--- include/rocksdb/write_batch.h
+++ include/rocksdb/write_batch.h
@@ -277,9 +277,7 @@ class WriteBatch : public WriteBatchBase {
   };
   Status Iterate(Handler* handler) const;
   class Iterator;
-  Iterator NewIterator() const {
-    return Iterator(this);
-  }
+  Iterator NewIterator() const { return Iterator(this); }
 
   // Retrieve the serialized version of this batch.
   const std::string& Data() const { return rep_; }
@@ -381,18 +379,25 @@ class WriteBatch : public WriteBatchBase {
    private:
     Slice content_;
     Slice input_;
-
-   public:
     Slice key_;
     Slice value_;
     uint32_t column_family_;
     char tag_;
     bool valid_;
 
+   public:
     explicit Iterator(const WriteBatch* wb)
         : content_(wb->rep_), valid_(false) {}
 
     bool Valid() const { return valid_; }
+
+    Slice Key() const { return key_; }
+
+    Slice Value() const { return value_; }
+
+    uint32_t GetColumnFamilyId() const { return column_family_; }
+
+    char GetValueType() const { return tag_; };
 
     void SeekToFirst();
 
