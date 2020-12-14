78b4fe45bca6d661784f5a6b32f3fba99f2dd70c
tabokie <xy.tao@outlook.com>
2020-12-14 14:21:29 +0800

    reset recent window
    Signed-off-by: tabokie <xy.tao@outlook.com>

--- utilities/rate_limiters/write_amp_based_rate_limiter.h
+++ utilities/rate_limiters/write_amp_based_rate_limiter.h
@@ -142,7 +142,7 @@ class WriteAmpBasedRateLimiter : public RateLimiter {
   };
 
   static constexpr size_t kSmoothWindowSize = 300;       // 300 * 1s = 5m
-  static constexpr size_t kRecentSmoothWindowSize = 40;  // 40 * 1s = 40s
+  static constexpr size_t kRecentSmoothWindowSize = 30;  // 30 * 1s = 30s
 
   WindowSmoother<kSmoothWindowSize, kRecentSmoothWindowSize> bytes_sampler_;
   WindowSmoother<kSmoothWindowSize, kRecentSmoothWindowSize>
