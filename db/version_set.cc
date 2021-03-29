e5e0f171346e16e1cba3d5513c80ba56b34289b4
CheneyDing <1017472599@qq.com>
2021-03-29 16:38:41 +0800

    add debug log

--- db/version_set.cc
+++ db/version_set.cc
@@ -2056,31 +2056,38 @@ void VersionStorageInfo::CalculateFileSizeRatioViolation(Logger* log, Version* v
     for (auto& file : files) {
       double violation = 0;
       Slice lower_bound(file->smallest.user_key());
-      PrintKey(file->smallest.user_key().data(), file->smallest.user_key().size());
-      PrintKey(file->largest.user_key().data(), file->largest.user_key().size());
+      //PrintKey(file->smallest.user_key().data(), file->smallest.user_key().size());
+      //PrintKey(file->largest.user_key().data(), file->largest.user_key().size());
       for (size_t i = 0; i < level_regions.num_regions; ++i) {
         // Skip regions that are smaller than current file
         if (user_comparator_->Compare(level_regions.regions[i].largest_user_key, file->smallest.user_key()) < 0) {
-          ROCKS_LOG_INFO(log, "Skip region %lu that are smaller that current file", i);
+          ROCKS_LOG_INFO(log, "Skip region %lu that are smaller that current file. File smallest key: [%lu, %s], region_largest_key: [%lu, %s]", 
+			  i, file->smallest.user_key().size(), file->smallest.user_key().data(), 
+			  level_regions.regions[i].largest_user_key.size(), level_regions.regions[i].largest_user_key.data());
           continue;
         }
         // find upper bound
         if (user_comparator_->Compare(file->largest.user_key(), level_regions.regions[i].largest_user_key) < 0 ||
             user_comparator_->Compare(file->largest.user_key(), level_regions.regions[i].largest_user_key) == 0) {
           // TODO(): need to compute a key range size in a sst file.
-          ROCKS_LOG_INFO(log, "Find upper bound of region %lu", i);
           InternalKey k1(lower_bound, kMaxSequenceNumber, kValueTypeForSeek);
           InternalKey k2(file->largest.user_key(), kMaxSequenceNumber, kValueTypeForSeek);
-          violation += vset->ApproximateSize(v, k1.Encode(), k2.Encode(), level, level + 1, TableReaderCaller::kUserApproximateSize)
-                       / file->compensated_file_size * level_regions.regions[i].size_ratio_violation;
+          uint64_t size = vset->ApproximateSize(v, k1.Encode(), k2.Encode(), level, level + 1, TableReaderCaller::kUserApproximateSize);
+	  double region_violation = level_regions.regions[i].size_ratio_violation;
+          violation += size / file->compensated_file_size * region_violation;
+	  ROCKS_LOG_INFO(log, "Find upper bound of region %lu --- lower_bound: [%lu, %s], upper_bound: [%lu, %s], size: %lu, file_size: %lu, region violation: %f, delta violation: %f", 
+			i, k1.user_key().size(), k1.user_key().data(), k2.user_key().size(), k2.user_key().data(), size, file->compensated_file_size, 
+			region_violation, violation);
           break;
         }
         InternalKey k1(lower_bound, kMaxSequenceNumber, kValueTypeForSeek);
         InternalKey k2(level_regions.regions[i].largest_user_key, kMaxSequenceNumber, kValueTypeForSeek);
         uint64_t size = vset->ApproximateSize(v, k1.Encode(), k2.Encode(), level, level + 1, TableReaderCaller::kUserApproximateSize);
-        violation += size / file->compensated_file_size * level_regions.regions[i].size_ratio_violation;
-	ROCKS_LOG_INFO(log, "lower_bound: [%lu, %s], upper_bound: [%lu, %s], size: %lu, violation: %f", 
-			k1.user_key().size(), k1.user_key().data(), k2.user_key().size(), k2.user_key().data(), size, violation);
+	double region_violation = level_regions.regions[i].size_ratio_violation;
+        violation += size / file->compensated_file_size * region_violation;
+	ROCKS_LOG_INFO(log, "Internal --- lower_bound: [%lu, %s], upper_bound: [%lu, %s], size: %lu, file_size: %lu, region %d violation: %f, delta violation: %f", 
+			k1.user_key().size(), k1.user_key().data(), k2.user_key().size(), k2.user_key().data(), size, file->compensated_file_size, 
+			i, region_violation, violation);
         lower_bound = level_regions.regions[i].largest_user_key;
         assert(level_regions.regions[i].largest_user_key == level_regions.regions[i].smallest_user_key);
       }
