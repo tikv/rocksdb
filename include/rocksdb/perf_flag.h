8dbd097fcd412a682afd5a6d648bec9c8660c25d
tabokie <xy.tao@outlook.com>
2021-08-12 14:15:17 +0800

    trigger rebuild
    Signed-off-by: tabokie <xy.tao@outlook.com>

--- include/rocksdb/perf_flag.h
+++ include/rocksdb/perf_flag.h
@@ -3,15 +3,17 @@
 
 #include <cstdint>
 
+#include "perf_flag_defs.h"
+
+#define GET_FLAG(flag) perf_flags[(uint64_t)(flag) >> (uint64_t)0b11]
+
 #define FLAGS_LEN                               \
   (((uint64_t)FLAG_END & (uint64_t)0b111) == 0  \
        ? ((uint64_t)FLAG_END >> (uint64_t)0b11) \
        : ((uint64_t)FLAG_END >> (uint64_t)0b11) + (uint64_t)1)
+
 namespace rocksdb {
 void EnablePerfFlag(uint64_t flag);
 void DisablePerfFlag(uint64_t flag);
 bool CheckPerfFlag(uint64_t flag);
 }  // namespace rocksdb
-
-#define GET_FLAG(flag) perf_flags[(uint64_t)(flag) >> (uint64_t)0b11]
-#include "perf_flag_defs.h"
