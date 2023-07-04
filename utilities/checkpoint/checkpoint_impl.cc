2fc2fc9e18edc1c1df61ad15735fbaecc2bf81aa
tabokie <xy.tao@outlook.com>
2023-07-04 07:48:18 +0200

    check compaction paused during checkpoint
    Signed-off-by: tabokie <xy.tao@outlook.com>

--- utilities/checkpoint/checkpoint_impl.cc
+++ utilities/checkpoint/checkpoint_impl.cc
@@ -310,7 +310,9 @@ Status CheckpointImpl::ExportColumnFamily(
   s = db_->GetEnv()->CreateDir(tmp_export_dir);
 
   if (s.ok()) {
-    s = db_->Flush(ROCKSDB_NAMESPACE::FlushOptions(), handle);
+    auto opts = ROCKSDB_NAMESPACE::FlushOptions();
+    opts.check_if_compaction_disabled = true;
+    s = db_->Flush(opts, handle);
   }
 
   ColumnFamilyMetaData db_metadata;
