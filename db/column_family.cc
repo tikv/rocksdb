83d29393bbd98781b39e8cd89169926549eee41f
Yang Zhang <yang.zhang@pingcap.com>
2024-01-02 20:43:00 -0800

    Apply comments
    Signed-off-by: Yang Zhang <yang.zhang@pingcap.com>

--- db/column_family.cc
+++ db/column_family.cc
@@ -51,12 +51,12 @@ ColumnFamilyHandleImpl::ColumnFamilyHandleImpl(
 }
 
 ColumnFamilyHandleImpl::ColumnFamilyHandleImpl(ColumnFamilyHandleImpl&& other) {
-  other.cfd_ = cfd_;
-  other.db_ = db_;
-  other.mutex_ = mutex_;
-  cfd_ = nullptr;
-  db_ = nullptr;
-  mutex_ = nullptr;
+  cfd_ = other.cfd_;
+  db_ = other.db_;
+  mutex_ = other.mutex_;
+  other.cfd_ = nullptr;
+  other.db_ = nullptr;
+  other.mutex_ = nullptr;
 }
 
 ColumnFamilyHandleImpl::~ColumnFamilyHandleImpl() {
